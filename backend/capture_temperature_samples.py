3b8a969df4ef216da29aa3986ee8a5985992f6c0
kszczupak <kszczupak00@gmail.com>
2018-11-22 15:13:55 +0100

    Added ability to detect command line args in capture_temperature_samples

--- backend/capture_temperature_samples.py
+++ backend/capture_temperature_samples.py
@@ -1,9 +1,17 @@
+import argparse
+
 from src.samples_grabber import SamplesGrabber
 
-if __name__ == '__main__':
-    grabber = SamplesGrabber()
-    grabber.fetch_samples(
-        cycle_interval=60,
-        samples_limit=2000,
-        bad_images_limit=20
+parser = argparse.ArgumentParser()
+parser.add_argument("-cycle_interval", "-i", type=int, help="Interval between pooling cycles in [sec]", default=60)
+parser.add_argument("-samples_limit", "-v", type=int, help="Limit of valid samples to gather", default=2000)
+parser.add_argument("-bad_samples_limit", "-b", type=int, help="Limit of not processed samples to gather", default=20)
+
+args = parser.parse_args()
+
+grabber = SamplesGrabber()
+grabber.fetch_samples(
+        cycle_interval=args.cycle_interval,
+        samples_limit=args.samples_limit,
+        bad_images_limit=args.bad_samples_limit
     )
