335774b6780e31629797ebd2df4a7fb17b9b143e
Krzysztof Szczupak <kszczupak00@gmail.com>
2019-03-23 23:33:25 +0000

    changed cropp paramters

--- backend/src/camera.py
+++ backend/src/camera.py
@@ -13,8 +13,8 @@ async def capture_image(image_file_name='image.jpg'):
         camera.resolution = resolution
         camera.rotation = 270
         camera.zoom = (
-            0.5,    # start x
-            0.468,  # start y
+            0.49,    # start x
+            0.463,  # start y
             0.031,  # width
             0.031   # height
         )
